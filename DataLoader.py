78f84c3025e4df2d8ca4bff891c54c2a4ed1fe5c
okiverny <oleg.kivernyk@gmail.com>
2025-02-03 14:34:15 +0100

    Improvement of MathProblemsDataset

--- DataLoader.py
+++ DataLoader.py
@@ -1,25 +1,30 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from typing import Dict, List
 
 class MathProblemsDataset:
     def __init__(self, dataset_name : str, partition_name: str):
         self.data_partition = partition_name
-        self.ds = load_dataset(dataset_name, split=partition_name)
+        self.__ds = load_dataset(dataset_name, split=partition_name).remove_columns("messages")
 
-    def get_data(self) -> dict:
-        return self.ds
+    @property
+    def data(self) -> Dataset:
+        return self.__ds
+    
+    @data.setter #property-data.setter decorator
+    def data(self, new_dataset: Dataset):
+        self.__ds = new_dataset
 
     def get_size(self) -> int:
-        return len(self.ds)
+        return len(self.__ds)
 
     def get_problem(self, idx : int) -> str:
-        return self.ds['problem'][idx]
+        return self.__ds['problem'][idx]
 
     def get_solution(self, idx : int) -> str:
-        return self.ds['solution'][idx]
+        return self.__ds['solution'][idx]
 
     def get_problems(self) -> List[str]:
-        return self.ds['problem']
+        return self.__ds['problem']
 
     def get_solutions(self) -> List[str]:
-        return self.ds['solution']
+        return self.__ds['solution']
